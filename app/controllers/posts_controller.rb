a684e7a2381d501538ba41881e8a67031dbae53d
haopang1 <hao.pang1@kellogg.northwestern.edu>
2022-02-21 08:39:57 +0000

    Corrected at 2/21

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+    def show
+        @post = Post.find(params["id"])
+      end
+    
+      def new
+        @post = Post.new
+        @post.company_id = params["place_id"]
+      end
+    
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
+      end
+    
+    
+end
